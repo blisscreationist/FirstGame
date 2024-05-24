553e58ed8418c259dc2e537db217c5430d2bff44
blisscreationist <101796532+blisscreationist@users.noreply.github.com>
2024-05-24 19:30:16 +0300

    add scores and color

---
+++ test.py
@@ -0,0 +1,48 @@
+import pygame
+import random
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Игра Тир")
+icon = pygame.image.load("img/marianeuro8_Picture_for_the_shooting_gallery_game_application_dcc5b952-14e1-41e3-a589-51e7e852eb47.png")
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("img/target.png")
+target_width = 50
+target_height = 50
+
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+
+font = pygame.font.Font(None, 36)
+
+score = 0
+color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+
+running = True
+while running:
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+                score += 1
+
+    screen.blit(target_img, (target_x, target_y))
+
+    score_text = font.render(f"Score: {score}", True, (255, 255, 255))
+    screen.blit(score_text, (10, 10))
+
+    pygame.display.update()
+
+pygame.quit()
