e3823f6363cb954e606983136e1289da2b858ff5
blisscreationist <101796532+blisscreationist@users.noreply.github.com>
2024-05-24 19:02:15 +0300

    add all const code

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ icon = pygame.image.load("img/marianeuro8_Picture_for_the_shooting_gallery_game_
 pygame.display.set_icon(icon)
 
 
-target_img = pygame.image.load("img/"target.png)
+target_img = pygame.image.load("img/target.png")
 target_width = 50
 target_height = 50
 
@@ -23,6 +23,17 @@ color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 
 running = True
 while running:
-    pass
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
 
 pygame.quit()
